066cce239ae87401d6a0999107a51c0a62dcad77
AnonymoxB <Anonymoxb12@gmail.com>
2025-02-12 23:35:40 +0700

    y

--- src/components/BigCalendar.tsx
+++ src/components/BigCalendar.tsx
@@ -1,37 +1,33 @@
-"use client";
+"use client"
 
-import { Calendar, momentLocalizer, View, Views } from 'react-big-calendar';
-import moment from 'moment';
+import { Calendar, momentLocalizer, View, Views } from 'react-big-calendar'
+import moment from 'moment'
 import { calendarEvents } from '@/lib/data';
-import "react-big-calendar/lib/css/react-big-calendar.css";
-import { useState, useEffect } from 'react';
+import "react-big-calendar/lib/css/react-big-calendar.css"
+import { useState } from 'react';
 
+moment.locale('en');
 
-
-const localizer = momentLocalizer(moment);
+const localizer = momentLocalizer(moment)
 
 const BigCalendar = () => {
-    const [view, setView] = useState<View>(Views.WORK_WEEK);
-
-    useEffect(() => {
-        moment.locale('en'); // ✅ Set locale hanya di client
-    }, []);
+    const[view, setView] = useState<View>(Views.WORK_WEEK);
 
     const handleOnChangeView = (selectedView: View) => {
         setView(selectedView);
-    };
+    } 
 
-    return (
-        <div>
+    return(
+        <div suppressHydrationWarning={true}>
             <Calendar
-                localizer={localizer}
-                events={calendarEvents}
-                startAccessor="start"
-                endAccessor="end"
-                views={["work_week", "day"]}
-                view={view}
-                style={{ height: 500 }}
-                onView={handleOnChangeView}
+            localizer={localizer}
+            events={calendarEvents}
+            startAccessor="start"
+            endAccessor="end"
+            views={["work_week", "day"]}
+            view={view}
+            style={{ height: 500 }}
+            onView={handleOnChangeView}
             />
         </div>
     );
