dc26f2aabc677a77c272cf16da46efa8a52b315c
bgsnnd <bgsnnd8@gmail.com>
2025-02-13 12:12:11 +0700

    update calender

--- src/components/BigCalendar.tsx
+++ src/components/BigCalendar.tsx
@@ -6,9 +6,15 @@ import { calendarEvents } from '@/lib/data';
 import "react-big-calendar/lib/css/react-big-calendar.css"
 import { useState } from 'react';
 
-moment.locale('en');
+// moment.locale('en');
 
 const localizer = momentLocalizer(moment)
+
+// Filter event hanya antara jam 8:00 pagi hingga 5:00 sore
+const filteredEvents = calendarEvents.filter(event => {
+    const eventStartHour = event.start.getHours();
+    return eventStartHour >= 8 && eventStartHour < 17;
+  });
 
 const BigCalendar = () => {
     const[view, setView] = useState<View>(Views.WORK_WEEK);
@@ -18,16 +24,18 @@ const BigCalendar = () => {
     } 
 
     return(
-        <div suppressHydrationWarning={true}>
+        <div>
             <Calendar
             localizer={localizer}
-            events={calendarEvents}
+            events={filteredEvents}
             startAccessor="start"
             endAccessor="end"
             views={["work_week", "day"]}
             view={view}
-            style={{ height: 500 }}
+            style={{ height: 900 }}
             onView={handleOnChangeView}
+            min={new Date(2026,0,0,8,0,0)}
+            max={new Date(2026,0,0,17,0,0)}
             />
         </div>
     );
