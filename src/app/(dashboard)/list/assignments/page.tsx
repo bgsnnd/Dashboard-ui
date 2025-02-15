74d797fe447649c8b6b73cc480bb644d8f5de049
bgsnnd <bgsnnd8@gmail.com>
2025-02-15 11:43:23 +0700

    formmodal ye

--- src/app/(dashboard)/list/assignments/page.tsx
+++ src/app/(dashboard)/list/assignments/page.tsx
@@ -1,3 +1,4 @@
+import FormModal from "@/components/FormModal"
 import Pagination from "@/components/Pagination"
 import Table from "@/components/Table"
 import TableSearch from "@/components/TableSearch"
@@ -50,15 +51,11 @@ const AssigmentListPage = () =>{
             <td className="hidden md:table-cell">{item.dueDate}</td>
             <td>
                 <div className="flex items-center gap-2">
-                    <Link href={'/list/teacher/${$item.id}'} >
-                        <button className="w-7 h-7 flex items-center justify-center rounded-full bg-lamaSky">
-                            <Image src="/edit.png" alt="" width={16} height={16} />
-                        </button>
-                    </Link>
                     { role === "admin" && (
-                        <button className="w-7 h-7 flex items-center justify-center rounded-full bg-lamaPurple">
-                        <Image src="/delete.png" alt="" width={16} height={16} />
-                        </button>
+                        <>
+                            <FormModal table="assignment" type="update" data={item}/>
+                            <FormModal table="assignment" type="delete" id={item.id}/>
+                        </>
                     )}
                 </div>
             </td>
@@ -83,9 +80,7 @@ const AssigmentListPage = () =>{
                         </button>
                     
                         { role === "admin" && (
-                            <button className="w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow">
-                            <Image src="/plus.png" alt="" width={14} height={14} />
-                            </button>
+                            <FormModal table="assignment" type="create"/>
                         )}
                         
                     </div>
--- src/app/(dashboard)/list/events/page.tsx
+++ src/app/(dashboard)/list/events/page.tsx
@@ -1,3 +1,4 @@
+import FormModal from "@/components/FormModal"
 import Pagination from "@/components/Pagination"
 import Table from "@/components/Table"
 import TableSearch from "@/components/TableSearch"
@@ -58,15 +59,11 @@ const EventListPage = () =>{
             <td className="hidden md:table-cell">{item.endTime}</td>
             <td>
                 <div className="flex items-center gap-2">
-                    <Link href={'/list/teacher/${$item.id}'} >
-                        <button className="w-7 h-7 flex items-center justify-center rounded-full bg-lamaSky">
-                            <Image src="/edit.png" alt="" width={16} height={16} />
-                        </button>
-                    </Link>
                     { role === "admin" && (
-                        <button className="w-7 h-7 flex items-center justify-center rounded-full bg-lamaPurple">
-                        <Image src="/delete.png" alt="" width={16} height={16} />
-                        </button>
+                        <>
+                            <FormModal table="event" type="update" data={item}/>
+                            <FormModal table="event" type="delete" id={item.id}/>
+                        </>
                     )}
                 </div>
             </td>
@@ -91,9 +88,7 @@ const EventListPage = () =>{
                         </button>
                     
                         { role === "admin" && (
-                            <button className="w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow">
-                            <Image src="/plus.png" alt="" width={14} height={14} />
-                            </button>
+                            <FormModal table="event" type="create"/>
                         )}
                         
                     </div>
